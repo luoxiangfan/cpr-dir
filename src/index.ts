e525fdb5134e28a8d85b523da88e23164e7f54e2
lxf <infiniment@163.com>
2024-11-29 14:43:54 +0800

    chore: optimize code to reduce the package size

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import async from 'async';
-import fs from 'node:fs';
-import nodePath from 'node:path';
+import {
+  statSync,
+  existsSync,
+  mkdirSync,
+  createReadStream,
+  createWriteStream
+} from 'node:fs';
 import readdirSyncRecursive from '@lxf2513/readdir-sync-recursive';
 
 function isFile(path: string) {
   try {
-    const stat = fs.statSync(path);
+    const stat = statSync(path);
     return stat.isFile();
   } catch (error) {
     return false;
@@ -14,7 +19,7 @@ function isFile(path: string) {
 
 function isDirectory(path: string) {
   try {
-    const stat = fs.statSync(path);
+    const stat = statSync(path);
     return stat.isDirectory();
   } catch (error) {
     return false;
@@ -28,24 +33,23 @@ function isDirectory(path: string) {
  * @param limit the max number of open files at a time
  */
 function copyDir(source: string, dest: string, limit: number | undefined) {
-  const list = readdirSyncRecursive(source);
-  const files = list.filter((l) => isFile(nodePath.join(source, l)));
-  const dirs = list.filter((l) => isDirectory(nodePath.join(source, l)));
+  const list = readdirSyncRecursive(source, 'relativePath');
+  const files = list.filter((l) => isFile(l));
+  const dirs = list.filter((l) => isDirectory(l));
   for (const dir of dirs) {
-    const destPath = nodePath.join(dest, dir);
-    if (!fs.existsSync(destPath)) {
-      fs.mkdirSync(destPath, { recursive: true });
-      copyDir(nodePath.join(source, dir), destPath, undefined);
+    const destPath = dir.replace(source, dest);
+    if (!existsSync(destPath)) {
+      mkdirSync(destPath, { recursive: true });
+      copyDir(dir, destPath, undefined);
     }
   }
   if (typeof limit !== 'number' || !files.length) {
     return;
   }
   async.eachLimit(files, limit, (file, next) => {
-    const srcPath = nodePath.join(source, file);
-    const destPath = nodePath.join(dest, file);
-    const readStream = fs.createReadStream(srcPath);
-    const writeStream = fs.createWriteStream(destPath);
+    const destPath = file.replace(source, dest);
+    const readStream = createReadStream(file);
+    const writeStream = createWriteStream(destPath);
     readStream.pipe(writeStream);
     writeStream.on('finish', next);
   });
